b19e9a635bc07eacff3f27369bf7287dcd38ec28
PSNapier <46052087+PSNapier@users.noreply.github.com>
2023-08-20 17:35:41 -0500

    feat: footer tweaks

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light">
     <ul class="navbar-nav ml-auto mr-auto">
-        <li class="nav-item"><a href="{{ url('rules') }}" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="{{ url('team') }}" class="nav-link">Credits</a></li>
+        <li class="nav-item"><a href="{{ url('rules') }}" class="nav-link">Rules</a></li>
+        <li class="nav-item"><a href="{{ url('team') }}" class="nav-link">Team</a></li>
         <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
         <li class="nav-item"><a href="{{ url('discord') }}" class="nav-link">Discord</a></li>
         <li class="nav-item"><a href="mailto:{{ env('CONTACT_ADDRESS') }}" class="nav-link">Contact Us</a></li>
