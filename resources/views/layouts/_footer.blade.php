2f32518118ceb52b6142a6bb97160d5f649cab95
PSNapier <46052087+PSNapier@users.noreply.github.com>
2023-08-20 17:46:29 -0500

    feat: footer tweaks

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-md navbar-light">
     <ul class="navbar-nav ml-auto mr-auto">
-        <li class="nav-item"><a href="{{ url('rules') }}" class="nav-link">Rules</a></li>
-        <li class="nav-item"><a href="{{ url('team') }}" class="nav-link">Team</a></li>
-        <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
-        <li class="nav-item"><a href="{{ url('discord') }}" class="nav-link">Discord</a></li>
-        <li class="nav-item"><a href="mailto:{{ env('CONTACT_ADDRESS') }}" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="{{ url('info/rules') }}" class="nav-link">Rules</a></li>
+        <li class="nav-item"><a href="{{ url('info/team') }}" class="nav-link">Team</a></li>
+        <li class="nav-item"><a href="{{ url('info/credits') }}" class="nav-link">Credits</a></li>
+        <li class="nav-item"><a href="{{ url('https://discord.com/') }}" class="nav-link">Discord</a></li>
+        <li class="nav-item"><a href="https://discord.com/" class="nav-link">Contact Us</a></li>
     </ul>
 </nav>
 <div class="copyright">Mythika-ARPG &copy; NovasEnd - All Rights Reserved {{ Carbon\Carbon::now()->year }}</div>
